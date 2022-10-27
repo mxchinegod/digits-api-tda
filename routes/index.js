e4b6dff00e64220e25b7cda670b2178d3130c5de
Dylan Moore <alloydylan@icloud.com>
2022-10-26 20:20:12 -0400

    Initial upload

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const express = require('express')
+
+/* A route that returns the health of the application. */
+router.get('/', (req, res) => {
+    var health = {};
+    var fs = require('fs'), obj
+    fs.readFile(`${__dirname}/../config.json`, handleFile)
+    function handleFile(err, data) {
+        if (err) { health.config({ "status": "unhealthy", "error": err }); return; }
+        obj = JSON.parse(data)
+        health.config = { "status": "healthy", "keys": Object.keys(obj).length }
+        health.isUp = {
+            uptime: process.uptime(),
+            message: 'OK',
+            date: new Date()
+        }
+        res.json(health);
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const config  = require("./config.json")
+const app = express()
+var cors = require('cors')
+app.use(cors())
+app.use(express.json())
+app.use("/",                require("./routes/index"))
+/* Listening on the port specified in the config.json file. */
+app.listen(config.servicePort,function(){
+    console.log(`Now listening on ${config.servicePort}`)
+})
