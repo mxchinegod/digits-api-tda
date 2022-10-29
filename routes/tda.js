c4fffb96445b0793726df37addfd1d68ff4bdea0
Dylan Moore <alloydylan@icloud.com>
2022-10-29 11:59:03 -0400

    added volatility map

--- routes/tda.js
+++ routes/tda.js
@@ -19,4 +19,36 @@ router.post('/options', function (req, res, next) {
     .end()
 });
 
+router.post('/volMap', function (req, res, next) {
+    const https = require("https");
+    https.request(`${config.tdaHost}/v1/marketdata/chains?apikey=${config.consumer}&symbol=${req.body.query.symbol.toUpperCase()}&contractType=${req.body.query.type}&strikeCount=${req.body.query.strikeCount}&includeQuotes=TRUE&fromDate=${req.body.query.fromDate}&toDate=${req.body.query.toDate}`, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        })
+        response.on('end', () => {
+            var jsonData = JSON.parse(data)["callExpDateMap"]
+            var final = {}
+            for (each in jsonData) {
+                const strikes = Object.keys(jsonData[each])
+                var strikeMap = strikes.map(strike=>{
+                    // each option from the strike
+                    var option = Object.values(jsonData[each][strike])[0]
+                    return strike = {
+                        vol: option.volatility
+                        , tVol: option.theoreticalVolatility
+                        , category: option.symbol
+                    }
+                })
+                final[each] = strikeMap
+            }
+            res.json(final)
+        })
+    })
+    .on('error', (error) => {
+        console.log('An error', error);
+    })
+    .end()
+});
+
 module.exports = router;
