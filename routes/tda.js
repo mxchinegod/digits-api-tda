3381abe81ef32c13bac3f18e48cf5dd6be9cbf47
Dylan Moore <alloydylan@icloud.com>
2022-10-31 17:43:02 -0400

    qol

--- routes/tda.js
+++ routes/tda.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const config  = require("../config.js")
+const config = require("../config.js")
 
 /* This is a route that is called when the user submits the form. It takes the form data and makes a
 request to the TDAmeritrade API. It then returns the data to the user. */
@@ -15,10 +15,10 @@ router.post('/options', function (req, res, next) {
             res.json(JSON.parse(data))
         })
     })
-    .on('error', (error) => {
-        console.log('An error', error);
-    })
-    .end()
+        .on('error', (error) => {
+            console.log('An error', error);
+        })
+        .end()
 });
 
 /* This is a route that is called when the user submits the form. It takes the form data and makes a
@@ -26,45 +26,58 @@ request to the TDAmeritrade API. It then returns the data to the user. */
 router.post('/volatility', function (req, res, next) {
     const https = require("https");
     const symbol = req.body.query.symbol.split('$')
-    https.request(`${config.tdaHost}/v1/marketdata/chains?apikey=${config.consumer}&symbol=${symbol.length>1?symbol[1].toUpperCase():symbol[0].toUpperCase()}&contractType=${req.body.query.type}&strikeCount=${req.body.query.strikeCount}&includeQuotes=TRUE&fromDate=${req.body.query.fromDate}&toDate=${req.body.query.toDate}`, (response) => {
+    https.request(`${config.tdaHost}/v1/marketdata/chains?apikey=${config.consumer}&symbol=${symbol.length > 1 ? symbol[1].toUpperCase() : symbol[0].toUpperCase()}&contractType=${req.body.query.type}&strikeCount=${req.body.query.strikeCount}&includeQuotes=TRUE&fromDate=${req.body.query.fromDate}&toDate=${req.body.query.toDate}&range=ITM`, (response) => {
         let data = '';
         response.on('data', (chunk) => {
             data = data + chunk.toString();
         })
         response.on('end', () => {
-            var jsonData = JSON.parse(data)["callExpDateMap"]
-            var final = {}
-            for (each in jsonData) {
-                const strikes = Object.keys(jsonData[each])
-                var strikeMap = strikes.map(strike=>{
-                    var option = Object.values(jsonData[each][strike])[0]
-                    return strike = {
-                        vol: option.volatility
-                        , tVol: option.theoreticalVolatility
-                        , category: option.symbol
+            const _ = JSON.parse(data)
+            if (_.status != 'FAILED') {
+                var expMap = _["callExpDateMap"]
+                var final = { strikeMap: {}, underlying: { mark: _.underlying["mark"], open: _.underlying["openPrice"], low: _.underlying["lowPrice"] } }
+                for (each in expMap) {
+                    const strikes = Object.keys(expMap[each])
+                    var strikeMap = strikes.map(strike => {
+                        var option = Object.values(expMap[each][strike])[0]
+                        return strike = {
+                            vol: parseInt(option.volatility)
+                            , tVol: option.theoreticalVolatility
+                            , strike: parseInt(strike)
+                            , category: option.symbol
+                            , openInterest: option.openInterest
+                        }
+                    })
+                    final.strikeMap[each] = strikeMap
+                }
+                let workingData = Object.values(final.strikeMap)[0]
+                workingData = workingData.slice().sort().reduceRight((acc, val, i) => {
+                    return i % 2 === 0 ? [...acc, val] : [val, ...acc];
+                }, [])
+                workingData.forEach((datum) => {
+                    datum.vol = (datum.vol.toString() * 1).toFixed(2)
+                    if (datum.vol == Math.min.apply(null, workingData.map((v) => { return v.vol }))) {
+                        datum.color = "#89c83d"
+                    } else {
+                        datum.color = "#7023cd"
                     }
                 })
-                final[each] = strikeMap
+                final.strikeMap = workingData;
+                final.pin = workingData.sort( 
+                    function(a, b) {
+                       return parseFloat(b['openInterest']) - parseFloat(a['openInterest']);
+                    }
+                  )[0]['strike'] 
+                res.json(final)
+            } else {
+                res.json({ "message": "Bad request" })
             }
-            let workingData = Object.values(final)[0]
-            workingData = workingData.slice().sort().reduceRight((acc, val, i) => {
-                return i % 2 === 0 ? [...acc, val] : [val, ...acc];
-            }, [])
-            workingData.forEach((datum) => {
-                datum.vol = (datum.vol.toString() * 1).toFixed(2)
-                if (datum.vol == Math.min.apply(null, workingData.map((v) => { return v.vol }))) {
-                    datum.color = "#89c83d"
-                } else {
-                    datum.color = "#7023cd"
-                }
-            })
-            res.json(workingData)
         })
     })
-    .on('error', (error) => {
-        res.send(error)
-    })
-    .end()
+        .on('error', (error) => {
+            res.send(error)
+        })
+        .end()
 });
 
 module.exports = router;
